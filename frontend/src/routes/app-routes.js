b62b3cc5239b1c868342d6d85448dbf351203358
ShashiniH <shshani0002@gmail.com>
2023-12-16 09:32:40 +0530

    first commit

---
+++ frontend/src/routes/app-routes.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import NavBar from "../components/navbar";
+import Footer from "../components/footer";
+
+import { } from "../pages";
+function App() {
+    return (
+        <></>
+    );
+}
+
+export default App;
